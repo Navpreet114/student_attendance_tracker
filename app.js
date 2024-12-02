83c3ffa72c073ad4d1423e04f3ca110d522be410
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-12-02 11:45:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080;
+const uri = process.env.MONGO_URI;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+//Connect the MongoDB
+mongoose.connect(uri).then(
+    async () => {
+        console.log('Connected to Mongo DB.');
+
+        app.listen(PORT, () => {
+            console.log(`Connected to Port ${PORT}`);
+        });
+    }
+).catch((err) => {
+    console.log(`Error connecting to database: ${err}`)
+});
