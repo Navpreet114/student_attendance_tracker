16b8dd95c34f4d18a3809c757b7d7e1c27dcd1dc
Navpreet114 <navpreet.singh@rdpolytech.ca>
2024-12-03 11:30:09 -0700

    first commit

--- app.js
+++ app.js
@@ -4,21 +4,27 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080;
 const uri = process.env.MONGO_URI;
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+const authRoutes = require('./routes/authRoutes.js');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//Serve the defination file
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.static('public'));
+app.use('/', authRoutes);
 
-//Connect the MongoDB
+
+//Connect to MongoDB
 mongoose.connect(uri).then(
-    async () => {
-        console.log('Connected to Mongo DB.');
-
-        app.listen(PORT, () => {
-            console.log(`Connected to Port ${PORT}`);
-        });
+    async () =>{
+      console.log('Connected to Mongo DB.');
+  
+      app.listen(PORT, () =>{
+        console.log(`Connected on port ${PORT}`);
+    });
     }
-).catch((err) => {
-    console.log(`Error connecting to database: ${err}`)
-});
+).catch((err) =>{ console.log(`Error connecting to database: ${err}`) });
